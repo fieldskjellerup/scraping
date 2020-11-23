cbba87f309d4025feab8a0e942cf73da3d30a059
Field Skjellerup <fieldskjellerup@gmail.com>
2020-11-23 17:48:15 +1300

    add cheerio load

--- scrape.js
+++ scrape.js
@@ -1,2 +1,12 @@
-const request = require ('request');
-const request = require ('cheerio');
+const request = require('request');
+const cheerio = require('cheerio');
+
+request('https://fieldskjellerup.github.io/index.html',(error, response, html) => {
+    if(!error && response.statusCode == 200) {
+        const $ = cheerio.load(html); 
+
+        const quote = $('.quote');
+
+        console.log(quote)
+    } 
+});
